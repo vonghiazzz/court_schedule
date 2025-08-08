b2c7d9f1e1e42761ff4f5805aa8a33ae40d99d1b
vonghiazzz <vonghia.votaan@gmail.com>
2025-08-08 17:36:17 +0700

    Edit .env again

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,4 @@
+# app/database.py đã sửa
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -5,18 +6,16 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
-
-APP_ENV = os.getenv("APP_ENV", "local")
-
-if APP_ENV == "docker":
-    DATABASE_URL = os.getenv("DATABASE_URL_DOCKER")
-elif APP_ENV == "production":
-    DATABASE_URL = os.getenv("DATABASE_URL")  # Railway sẽ tự set biến này
-else:
-    DATABASE_URL = os.getenv("DATABASE_URL_LOCAL")
-
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    APP_ENV = os.getenv("APP_ENV", "local")
+    if APP_ENV == "docker":
+        DATABASE_URL = os.getenv("DATABASE_URL_DOCKER")
+    else:
+        DATABASE_URL = os.getenv("DATABASE_URL_LOCAL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set. Check your .env file or environment variables.")
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
