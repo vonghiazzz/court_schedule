21d2fcb6617248572b06799823563ac8e4ff9d96
vonghiazzz <votannghia.nt18@gmail.com>
2025-08-16 16:24:03 +0700

    Edit env and database again

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,19 +7,21 @@ from dotenv import load_dotenv
 # Luôn load biến từ file .env để đảm bảo
 load_dotenv()
 
-APP_ENV = os.getenv("APP_ENV", "local")
+# APP_ENV = os.getenv("APP_ENV", "local")
 
-if APP_ENV == "local":
-    DATABASE_URL = os.getenv("DATABASE_URL_LOCAL")
-elif APP_ENV == "docker":
-    DATABASE_URL = os.getenv("DATABASE_URL_DOCKER")
-elif APP_ENV == "production":
-    DATABASE_URL = os.getenv("DATABASE_URL_PRODUCTION")  # Render/Railway cấp
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-else:
-    DATABASE_URL = None
-print("APP_ENV:", os.getenv("APP_ENV"))
-print("DATABASE_URL:", os.getenv("DATABASE_URL"))
+# if APP_ENV == "local":
+#     DATABASE_URL = os.getenv("DATABASE_URL_LOCAL")
+# elif APP_ENV == "docker":
+#     DATABASE_URL = os.getenv("DATABASE_URL_DOCKER")
+# elif APP_ENV == "production":
+#     DATABASE_URL = os.getenv("DATABASE_URL_PRODUCTION")  # Render/Railway cấp
+
+# else:
+#     DATABASE_URL = None
+# print("APP_ENV:", os.getenv("APP_ENV"))
+# print("DATABASE_URL:", os.getenv("DATABASE_URL"))
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set. Check your environment variables.")
