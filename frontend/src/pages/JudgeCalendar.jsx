f09aeee86b8907a142fb9b48398eb5040993e60d
vonghiazzz <votannghia.nt18@gmail.com>
2025-08-27 16:35:53 +0700

    Edit name avairable juror

--- frontend/src/pages/JudgeCalendar.jsx
+++ frontend/src/pages/JudgeCalendar.jsx
@@ -9,7 +9,7 @@ const ROOMS = ["Hội trường 1", "Hội trường 2", "Hội trường 3", "H
 const SHIFTS = ["Sáng", "Chiều","Cả ngày"];
 const WEEKDAYS = ["CN", "T2", "T3", "T4", "T5", "T6", "T7"];
 const MONTHS = ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"];
-const JURORS = [
+const JUROR = [
   "Lê Thị Tú Anh",
   "Ngụy Mộng Cầm",
   "Nguyễn Tùng Châu",
